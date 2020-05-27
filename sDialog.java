3a796f855417ad741c19191ad26c15b85914b9f5
ShinHyeongSeo <62292136+ShinHyeongSeo@users.noreply.github.com>
2020-05-27 13:09:18 +0900

    Update reset record

--- sDialog.java
+++ sDialog.java
@@ -9,6 +9,7 @@ import javax.swing.JDialog ;
 class sDialog extends JDialog{
 	private JButton ok ;
 	private JButton no ;
+	private manage_data manage = new manage_data();
 	
 	public sDialog(JFrame win, String title)
 	{
@@ -23,6 +24,7 @@ class sDialog extends JDialog{
 
 		ok.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e){
+				manage.delete_all_score(); // 모든 기록 초기화
 				setVisible(false) ; // reset record
 			}
 		}) ;
