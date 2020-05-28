8df8f83aa3eed8b4185433f87f4c99b7635117c7
kongsubin <qls0919@naver.com>
2020-05-28 22:32:20 +0900

    key

--- panel/Game.java
+++ panel/Game.java
@@ -1,4 +1,5 @@
 package panel;
+import java.awt.Color;
 import java.awt.event.ActionEvent ;
 import java.awt.event.ActionListener ;
 import javax.swing.JButton;
@@ -12,18 +13,25 @@ public class Game extends JPanel{
 	private JButton jButton ;
 	private Main_Frame win ;
 	private RunGame running;
+	JPanel p;
+	int x = 0, y = 0;
 
 	public Game(Main_Frame win){
 			this.win = win ;
 			setLayout(null) ;
-
+			
 			jButton = new JButton("Back") ;
 			jButton.setSize(50, 20) ;
 			jButton.setLocation(490, 450) ;
 			add(jButton) ;
-
 			jButton.addActionListener(new MyActionListener()) ;
-			running = new RunGame();
+			
+			p = new JPanel();
+			p.setBounds(100, 100, 50, 50);
+			p.setBackground(Color.RED);
+			add(p);
+			
+			running = new RunGame(win, p);
 	}
 
 	class MyActionListener implements ActionListener{
--- thread/RunGame.java
+++ thread/RunGame.java
@@ -1,26 +1,124 @@
 package thread;
-public class RunGame implements Runnable{
-	Thread th; 
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import main.Main_Frame;
+import panel.Game;
 
-	public RunGame(){
+public class RunGame extends JFrame implements Runnable, KeyListener{
+	Thread th; 
+	Main_Frame win;
+	Game game;
+	JPanel p;
+	int x = 0, y = 0; 
+	 
+	boolean KeyUp = false;
+	boolean KeyDown = false;
+	boolean KeyLeft = false;
+	boolean KeyRight = false;
+	boolean KeySpace = false;
+
+	public RunGame(Main_Frame win, JPanel p){
+			this.win = win;
+			this.p = p;
+			win.addKeyListener(this);
+			win.setFocusable(true);
 			th = new Thread(this); 
 			th.start(); 
 	}
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		System.out.println("thread create!");
 		try { 
 			while (true) { 
-				//repaint();
+				KeyProcess();
+				p.setLocation(x,y);
+				repaint();
 				Thread.sleep(20); 
 			}
 		} catch (Exception e) {
 		}
 	}
+	
 	public void kill() {
 		System.out.println("thread kill!");
 		th.interrupt();
+	}
+	
+	private void KeyProcess() {
+		// TODO Auto-generated method stub
+		if(KeyUp == true)  {
+			System.out.println("up");
+			y -= 5;
+		}
+		if(KeyDown == true) {
+			System.out.println("down");
+			y += 5;
+		}
+		if(KeyLeft == true)  {
+			System.out.println("left");
+			x -= 5;
+		}
+		if(KeyRight == true) {
+			System.out.println("right");
+			x += 5;
+		}
+		if(KeySpace == true) {
+			System.out.println("attack");
+		}
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			KeyUp = true;
+			break;
+		case KeyEvent.VK_DOWN:
+			KeyDown = true;
+			break;
+		case KeyEvent.VK_LEFT:
+			KeyLeft = true;
+			break;
+		case KeyEvent.VK_RIGHT:
+			KeyRight = true;
+			break;
+		case KeyEvent.VK_SPACE:
+			KeySpace = true;
+			break;
+		}
+
+	}
+	
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			KeyUp = false;
+			break;
+		case KeyEvent.VK_DOWN:
+			KeyDown = false;
+			break;
+		case KeyEvent.VK_LEFT:
+			KeyLeft = false;
+			break;
+		case KeyEvent.VK_RIGHT:
+			KeyRight = false;
+			break;
+		case KeyEvent.VK_SPACE:
+			KeySpace = false;
+			break;
+		}
 	}
 	
 }	
