ea9cbc1266528bb38285a6f078b4ca8feb06a67c
kongsubin <qls0919@naver.com>
2020-05-30 21:49:57 +0900

    Missile

---
+++ panel/Missile.java
@@ -0,0 +1,17 @@
+package panel;
+
+
+public class Missile {
+	public int x;
+	public int y; // 편의상 변수 명 변경
+
+	public Missile(int x, int y) {
+		this.x = x;
+		this.y = y;// 편의상 변수명 변경
+	}
+
+	public void move() {
+		x += 10;
+	}
+	
+}
