d6eec237441368ffb59ddb1e009f483e88131c2c
kongsubin <qls0919@naver.com>
2020-06-04 23:52:27 +0900

    add setX, setY

--- panel/Missile.java
+++ panel/Missile.java
@@ -14,8 +14,24 @@ public class Missile {
 	public void move() {
 		x += 30;
 	}
+
 	public void enemy_move(int speed) { // enemy's missile move
 		x -= speed;
 	}
-	
+
+	public int getX() {
+		return this.x;
+	}
+
+	public int getY() {
+		return this.y;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int x) {
+		this.y = y;
+	}
 }
